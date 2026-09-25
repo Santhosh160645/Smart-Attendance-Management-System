3b3f63ea5c874891fe6f185f9d18d3edd2093524
Santhosh Vaskula <157871527+Santhosh160645@users.noreply.github.com>
2026-09-25 09:12:55 +0530

    Add files via upload

---
+++ database/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite:///./attendance.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
